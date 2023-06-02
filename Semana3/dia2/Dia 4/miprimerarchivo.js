ced7016a3f92709cf6bddc6fe1feab134fac2d16
grinpow <piero2086gmail.com>
2023-06-02 12:12:00 -0500

    actualizando

---
+++ Semana3/dia2/Dia 4/miprimerarchivo.js
@@ -0,0 +1,28 @@
+// variablessss
+// tipos de datoss
+// var let const
+
+let numero1 = 10
+let numero2 = 20
+
+let resultado = 0;
+
+resultado = numero1 + numero2;
+
+console.log("la suma es ",resultado)
+
+
+
+let nombreComprador = "Piero"
+let nombreEntrada = "Mario Bros"
+let precioEntrada = "10 soles"
+
+
+console.log("soy " + nombreComprador + " he comprado entradas para " + nombreEntrada + " a " + precioEntrada )
+
+
+let horarioPelicula = "4:00 pm "
+let opcionPelicula = "Titanic"
+let numeroAsiento = "22A"
+
+console.log("Fui a ver " + opcionPelicula + " a las " + horarioPelicula + "en el asiento " + numeroAsiento)
